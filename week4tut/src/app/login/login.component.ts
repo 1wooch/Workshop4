57cf2f5b422e58d78f24c60db7937926d1e24e57
Wonwoo Choi <onechoo1106@gmail.com>
2022-08-12 13:39:12 +1000

    FInal week4

--- week4tut/src/app/login/login.component.ts
+++ week4tut/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Route } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   username="";
   password="";
+  Userlist=[{id:"wonwoo@edu.au",user_password:"991106"},{id:"test@edu.au",user_password:"1111"},{id:"test2@edu.au",user_password:"2222"}];
+  //Userlist=new Dictionary();
 
-
-  constructor() { }
+  constructor(private route :Router) { }
 
   ngOnInit(): void {
   }
@@ -18,6 +20,26 @@ export class LoginComponent implements OnInit {
     //alert("Hi");
     //alert(this.username);
     //alert(this.password);
+    for (let i=0; i<this.Userlist.length; i++){
+      if(this.Userlist[i]['id']==this.username){
+      //alert(this.Userlist[i]['id']);
+      //alert(true);
+          if(this.Userlist[i]['user_password']==this.password){
+            alert("Login Successful");
+            this.route.navigateByUrl('/account');
+            break;
+          }
+          else{
+            alert("Wrong Password");
+            break;
+          }
+      }
+      else{
+        alert(
+          "Check the ID and Password"
+        );
+      }
+    }
   }
 
 }
