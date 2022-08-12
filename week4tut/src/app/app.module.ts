b83fc4ecd68e1730b0f1d1962a78aaaf7c489af6
Wonwoo Choi <onechoo1106@gmail.com>
2022-08-12 10:06:12 +1000

    WORKINON LOGIN

--- week4tut/src/app/app.module.ts
+++ week4tut/src/app/app.module.ts
@@ -2,15 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
+
+import { Router, RouterModule, Routes } from '@angular/router';
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppRougingModule } from './app-rouging.module';
+
+import { FormsModule } from '@angular/forms';
+
+
+const routes: Routes=[
+  {path:'login',component:LoginComponent},
+  {path:'account',component:AccountComponent},
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AccountComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterTestingModule,
+    AppRougingModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[RouterModule]
 })
 export class AppModule { }
